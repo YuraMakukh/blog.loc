86a8d07e18b9b3e08cd85c89c4df912b2d7bf3ed
Yura Makukh <yuramakukh1982@gmail.com>
2023-02-09 21:54:03 +0200

    Add categoryAdd-viev lesson6

---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,25 @@
+
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class=" pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+                <li class="nav-item">
+                    <a href="{{route('admin.category.index')}}" class="nav-link">
+                        <i class="nav-icon fa-solid fa-bars"></i>
+                        <p>
+                            Gategories
+                        </p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
